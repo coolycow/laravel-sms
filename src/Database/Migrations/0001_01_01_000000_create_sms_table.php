32eb9cfa91ed31c10282b1e81190c2c41e6f08a8
coolycow <coolycow.com@gmail.com>
2025-10-07 11:50:42 +0300

    fix migration

--- src/Database/Migrations/0001_01_01_000000_create_sms_table.php
+++ src/Database/Migrations/0001_01_01_000000_create_sms_table.php
@@ -4,8 +4,9 @@ namespace Coolycow\LaravelSms\Database\Migrations;
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateSmsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
